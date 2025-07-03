5a4f165ff275aee48488df3b6ddf23aa6a3b8548
Arthur <guy.art123.ad@gmail.com>
2025-07-03 11:19:26 -0300

    arrumando responsividade

--- anais.php
+++ anais.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Anais - V MARAPET</title>
-  
-  <!-- Link para o arquivo CSS -->
-  <link rel="stylesheet" href="css/monitoria.css"> <!-- Substitua pelo seu caminho -->
-</head>
-<body>
-
 <?php
   require_once('html_header.php');
+?>
+
+<!-- Adiciona o CSS específico somente nesta página -->
+<link rel="stylesheet" href="css/monitoria.css">
+
+<?php
   require_once('header.php');
 ?>
+
   <div class="section-header">
   <h1 id="centered">ANAIS</h1>
             </div>
--- inscricoes.php
+++ inscricoes.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inscrições - V MARAPET</title>
-  
-  <!-- Link para o arquivo CSS -->
-  <link rel="stylesheet" href="css/monitoria.css"> <!-- Substitua pelo seu caminho -->
-</head>
-<body>
-
 <?php
   require_once('html_header.php');
+?>
+
+<!-- Adiciona o CSS específico somente nesta página -->
+<link rel="stylesheet" href="css/monitoria.css">
+
+<?php
   require_once('header.php');
 ?>
 
